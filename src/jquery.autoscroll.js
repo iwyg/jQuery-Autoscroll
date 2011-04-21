0c53604567a141d1f1d52bec33a365aa8c82d341
thomas appel <mail@thomas-appel.com>
2011-04-21 17:52:31 +0200

    fixed some minor issues

--- src/jquery.autoscroll.js
+++ src/jquery.autoscroll.js
@@ -1,3 +1,47 @@
+/**
+ *
+ * jQuery Autoscroll: an autoscrolling Plugin for the jQuery Javascript Library
+ * --------------------------------------------------------------------------------------------
+ * usage: 
+ * 
+ * - create html container which contains something like a unordered list
+ * - setup plugin like this: $(mycontainer).autoscroll();
+ * 
+ * --------------------------------------------------------------------------------------------
+ * options: 
+ * 
+ * - scrollOnClick : boolean; if set, you my create navigation "links" for back and forward scrolling
+ *	 ### These links must not be children of the container element. ###
+ *	 ### Also note that scrollOnClick is only useful/working if the scrollable content (e.g. a html List Element) has equal heights/width
+ *	 ### Typically set your container to 600px width if your list items are 200px wide each, and you want to show 3 items at one time.
+ *	 
+ * - navNext : navigation element for scrolling forward  // jQuery Object
+ * - navPrev : navigation element for scrolling backward // jQuery Object
+ * - scrollOnClickEasing : easing method for scrollOnClick option // default 'swing'
+ * - scrollOnClickDuration : scrollanimation duration for scrollOnClick option
+ * 
+ * --------------------------------------------------------------------------------------------
+ * misc:
+ * --------------------------------------------------------------------------------------------
+ * - camptible with jquery.events.destroyed
+ * - plugin will detroy itself if container is removed from DOM
+ * - if you want di teardown the plugin manually, just call: $(mycontainer).data('autoscroll').destroy();
+ *	 or simply $(mycontainer).trigger('destroyed');
+ * --------------------------------------------------------------------------------------------
+ * revision: 2
+ * --------------------------------------------------------------------------------------------
+ * 
+ * known issues:
+ * --------------------------------------------------------------------------------------------
+ * - removed support for tables
+ * --------------------------------------------------------------------------------------------
+ * @name jquery.autoscroll.js
+ * @author thomas appel, mail(at)thomas-appel(dot)com
+ * @copyright thomas appel
+ * @version 1
+ */
+
+
 (function ($){
 
 	var propertyStack = {
@@ -133,18 +177,20 @@
 		scrollStepWise : function( e ) {
 			e.preventDefault();
 			var that = this, anim = {},	val, 
-			w = this.elem[propertyStack.INNER_W_H[this.orient]](),
-			s = this.elem[0][propertyStack.SCROLL[this.orient]],			
-			ss = this.elem[0][propertyStack.SCROLL_W_H[this.orient]],			
+			w = this.elem[propertyStack.INNER_W_H[this.orient]](),			// scroll container inner width or height
+			s = this.elem[0][propertyStack.SCROLL[this.orient]],			// current scroll offset
+			ss = this.elem[0][propertyStack.SCROLL_W_H[this.orient]],		// overall scroll width	or height
 			// sup = ss % Math.abs(w-s) || 0;
 			sup = w > s ? w%s : s%w;
 			if ( e.target === this.settings.navNext[0]) {
-				val = ( isNaN(sup) || sup === 0 ) ? ( w + s ) : s < w ? w : Math.ceil(s/w) * w;				
+			//	val = ( isNaN(sup) || sup === 0 ) ? ( w + s ) : s < w ? w : Math.ceil(s/w) * w;				
+				val = ( isNaN(sup) || sup === 0 ) ? ( w + s ) : Math.ceil(s/w) * w;				
 			}
 			if ( e.target === this.settings.navPrev[0]) {
-				val = ( isNaN(sup) || sup === 0 ) ? ( s - w ) : s < w ? 0 : (Math.ceil(s/w) * w) - w;				
+				//val = ( isNaN(sup) || sup === 0 ) ? ( s - w ) : s < w ? 0 : (Math.ceil(s/w) * w) - w;				
+				val = ( isNaN(sup) || sup === 0 ) ? ( s - w ) : (Math.ceil(s/w) * w) - w;				
 			}
-			anim[propertyStack.SCROLL[this.orient]] = val;
+			anim[propertyStack.SCROLL[this.orient]] = Math.max(Math.min(val, ss-w),0);
 			this.elem.animate(anim, this.settings.scrollOnClickDuration * 1000, this.settings.scrollOnClickEasing );	
 			
 		},		
